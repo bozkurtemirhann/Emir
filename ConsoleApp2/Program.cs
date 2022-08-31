a44895809f442e4fd2b39c77f2be228d107771f9
Emirhan Bozkurt <111983732+bozkurtemirhann@users.noreply.github.com>
2022-09-01 00:09:47 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+           // KM Sistemi. 
+            
+            Console.WriteLine("KM değer giriniz. ");
+            int km;
+
+            km = (Convert.ToInt32(Console.ReadLine()));
+
+            double perKM = 2.20, total = 10;
+
+            total += (perKM * km);
+
+            total = (total < 20) ? 20 : total;
+
+            Console.WriteLine("Toplam Tutar: " + total);
+            Console.ReadKey();
+        } 
+
+    }
+}
+
