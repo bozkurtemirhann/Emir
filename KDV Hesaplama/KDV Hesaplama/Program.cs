ef9be559034737074c7e19198f53b2ad657b0f58
Emirhan Bozkurt <111983732+bozkurtemirhann@users.noreply.github.com>
2022-09-09 14:38:27 +0300

    KDV hesaplama

---
+++ KDV Hesaplama/KDV Hesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // KDV Hesaplama
+            double kdvoran = 0.18, kdvTutar, kdvLiTutar;
+            Console.WriteLine("Ucret tutarını giriniz: ");
+            double tutar = Convert.ToInt32(Console.ReadLine());
+
+
+            kdvTutar = tutar * kdvoran;
+            kdvLiTutar = tutar + kdvTutar;
+            Console.WriteLine("KDVSİZ TUTAR: " + tutar);
+            Console.WriteLine("KDV ORANI: " + kdvoran);
+            Console.WriteLine("KDV TUTARI: " + kdvTutar);
+            Console.WriteLine("KDVLİ TUTAR: " + kdvLiTutar);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
